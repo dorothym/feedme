7a695394be9e6b43d8b50775f7924303b2c566d2
dorothym <dorothy.v.moore@gmail.com>
2016-03-01 12:30:10 -0500

    add \'status\' field to transaction schema and fixed typos

--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -5,9 +5,12 @@ var Schema = mongoose.Schema;
 
 var transactionSchema = new Schema({
 	// do we need to add sessionId?
-	customerId:  {type: Schema.Types.ObjectId, ref: 'User'},
+	customerId:  {type: Schema.Types.ObjectId, ref: 'User'}, 
+	// this is not required initially. could be anonymous user just browsing. 
+	// will need to check customer ID is present before allowing user to check out
 	mealPackageIds: [{type: Schema.Types.ObjectId, ref: 'MealPackage'}],
 	totalprice: {type: Number},
+	status: {type: String} // options: pending (if customer is still shopping) OR checkout (if during checkout) OR complete (if order has been placed)
 });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
