88567c143431cb3092c6949ff5830681f333421e
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-01 22:16:45 -0500

    added transaction route

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/transactions', require('./transactions'));
 
 // Make sure this is after all of
 // the registered routes!
