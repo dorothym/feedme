27366197c39e15c28f2ecccafa01560ca1715dc1
dorothym <dorothy.v.moore@gmail.com>
2016-03-02 10:57:41 -0500

    working on adding enum to transaction schema

--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -3,6 +3,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var transactionStates = ['Created', 'Processing', 'Cancelled', 'Completed'];
+
 var transactionSchema = new Schema({
 	customerId:  {type: Schema.Types.ObjectId, ref: 'User'}, 
 	totalprice: {type: Number},
