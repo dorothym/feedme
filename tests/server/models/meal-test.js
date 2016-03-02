b095f5456aef6b6e8d86ab9d7b703329eb729adc
dorothym <dorothy.v.moore@gmail.com>
2016-03-02 17:23:32 -0500

    tests are working

--- tests/server/models/meal-test.js
+++ tests/server/models/meal-test.js
@@ -5,6 +5,8 @@ var sinon = require('sinon');
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
 
+var Promise = require('bluebird'); 
+
 // Require in all models.
 require('../../../server/db/models');
 
@@ -33,7 +35,8 @@ describe('Meal model', function () {
 
         var newMeal;
 
-        before(function(done) {
+        // use done with error first callbacks 
+        beforeEach(function(done) {
           newMeal = new Meal({
             name:  'Vegetable Lasagna',
             cuisine: 'Italian',
@@ -42,8 +45,45 @@ describe('Meal model', function () {
             diet: 'Vegetarian',
             servings: 2
           });
-          return newMeal.save(done);
+          newMeal.save(function(err, meal){
+            if (err) return done(err)
+            else done()
+          });
+
         });
+
+
+        // // use done with promises 
+        // beforeEach(function(done) {
+        //    Meal.create({
+        //     name:  'Vegetable Lasagna',
+        //     cuisine: 'Italian',
+        //     description: 'A savory dish from southern Italy, with fresh summer vegtables',
+        //     price: 10,
+        //     diet: 'Vegetarian',
+        //     servings: 2
+        //     })
+        //     .then(function(){
+        //         done()
+        //     })
+        //     .then(null, done)
+        // });
+
+
+
+        // // // return promise pattern 
+        // beforeEach(function() {
+        //   return Meal.create({
+        //     name:  'Vegetable Lasagna',
+        //     cuisine: 'Italian',
+        //     description: 'A savory dish from southern Italy, with fresh summer vegtables',
+        //     price: 10,
+        //     diet: 'Vegetarian',
+        //     servings: 2
+        //   });
+
+        // });
+
 
 
         it('should successfully create a meal', function(done){
@@ -60,51 +100,58 @@ describe('Meal model', function () {
 
      describe('getChef method', function () {
 
-        before(function(done) {
-            var meal1,meal2,
-                chef1,chef2;
+         var chef1,chef2, meal1, meal2
+        beforeEach(function(done) {
 
-                Meals = [
+           Promise.all([
+                Meal.create({
+                    name: 'Awesome chicken meal',
+                    cuisine: 'Indian',
+                    description: 'An amazing dish',
+                    price: 8,
+                    servings: 1
+                  }), 
+                Meal.create({
+                    name: 'Lasagna',
+                    cuisine: 'Italian',
+                    description: 'A savory dish from southern Italy, with fresh summer vegtables',
+                    price: 10,
+                    diet: 'Vegetarian',
+                    servings: 2
+                  })
+            ])
+           .spread(function(_meal1, _meal2){
+                meal1 = _meal1
+                meal2 = _meal2
+                done()
+           })
+           .then(null, done)
 
-              {
-                name: 'Awesome chicken meal',
-                cuisine: 'Indian',
-                description: 'An amazing dish',
-                price: 8,
-                servings: 1
-              },
+        })
 
-              {
-                name: 'Lasagna',
-                cuisine: 'Italian',
-                description: 'A savory dish from southern Italy, with fresh summer vegtables',
-                price: 10,
-                diet: 'Vegetarian',
-                servings: 2
-              }
-            ]
+        beforeEach(function(done){
+            Chef.create({
+                email: 'chef1@gmail.com',
+                password: '123',
+                firstName: 'Frank',
+                lastName: 'Chef',
+                borough: 'Queens',
+                specialty: 'Indian',
+                bio: 'Please eat my food!',
+                meals: [meal1, meal2]
+            })
+            .then(function(){
+                done()
+            })
+            .then(null, done)
+        })
 
-            return Meal.create(Meals)
-            .then(function (mealArray) {
-                console.log("line 92", mealArray)
-                 return Chef.create({
-                    email: 'chef1@gmail.com',
-                    password: '123',
-                    firstName: 'Frank',
-                    lastName: 'Chef',
-                    borough: 'Queens',
-                    specialty: 'Indian',
-                    bio: 'Please eat my food!',
-                    meals: [mealArray[0]._id, mealArray[1]._id]
-                });
-              });
-        });
 
         it('should return chef associated with a meal ID', function(done){
             meal1.getChef()
             .then(function(chef){
                 console.log("inside .then returning chef")
-              expect(chef).firstName.to.equal('Frank');
+              expect(chef.firstName).to.equal('Frank');
               done();
             })
             .then(null, done);
