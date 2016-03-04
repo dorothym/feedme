32c1169e5e35d409eaab49852922d01bf6aa490a
dorothym <dorothy.v.moore@gmail.com>
2016-03-03 20:43:19 -0500

    Additional design work
    - Mocked up single meal (/meal)
     -- modified left sidebar to show categories and price
     -- display chef name
    
    - connected some navbar links to dummy html routes
    
    - Moved homepage template to actual route on homepage
    
     -- homepage is not rendering correctly. Need to trobuleshoot. Could be CSS issue? home.scss doesn't seem to be getting digested into main.css by gulp

--- browser/js/meals/meals.js
+++ browser/js/meals/meals.js
@@ -19,3 +19,23 @@ app.controller('AllMealsController', function ($scope) {
     // $scope.images = _.shuffle(FullstackPics);
 
 });
+
+app.config(function ($stateProvider) {
+
+    // Register our *about* state.
+    $stateProvider.state('meal', {
+        url: '/meal',
+        controller: 'MealController',
+        templateUrl: 'js/meals/meal.html'
+    });
+
+});
+
+app.controller('MealController', function ($scope) {
+
+	$scope.angularTest = "hooray!"
+
+    // Images of beautiful Fullstack people.
+    // $scope.images = _.shuffle(FullstackPics);
+
+});
