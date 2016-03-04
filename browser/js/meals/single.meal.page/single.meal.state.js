734a2d7119252d6da6ea12d16d191683d87a5ba6
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-04 16:52:56 -0500

    slight changes

--- browser/js/meals/single.meal.page/single.meal.state.js
+++ browser/js/meals/single.meal.page/single.meal.state.js
@@ -1,15 +1,15 @@
-app.config(function ($stateProvider) {
-
-    // Register our *about* state.
-    $stateProvider.state('meal', {
-        url: '/meals/:id',
-        controller: 'MealController',
-        templateUrl: 'js/meals/single.meal.page/meal.html',
-        resolve: {
-          meal: function(SingleMeal, $stateParams){
-            return SingleMeal.getMeal($stateParams.id);
-          }
-        }
-    });
-
-});
+//app.config(function ($stateProvider) {
+//
+//    // Register our *about* state.
+//    $stateProvider.state('meal', {
+//        url: '/meals/:id',
+//        controller: 'MealController',
+//        templateUrl: 'js/meals/single.meal.page/meal.html',
+//        resolve: {
+//          meal: function(SingleMeal, $stateParams){
+//            return SingleMeal.getMeal($stateParams.id);
+//          }
+//        }
+//    });
+//
+//});
