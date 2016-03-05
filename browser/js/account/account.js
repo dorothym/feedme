06dec7d44e6c1201347dc6f8e2c8b2030403dde3
dorothym <dorothy.v.moore@gmail.com>
2016-03-05 17:25:15 -0500

    added Welcome link on navbar that links to stub User / My Account page

---
+++ browser/js/account/account.js
@@ -0,0 +1,27 @@
+app.config(function($stateProvider) {
+    $stateProvider.state('account', {
+        url: '/account',
+        templateUrl: 'js/account/account.html',
+        controller: 'AccountCtrl'
+    });
+})
+
+app.controller('AccountCtrl', function($scope, $rootScope, AuthService) {
+
+   $scope.user = null;
+
+   $scope.isLoggedIn = function () {
+        return AuthService.isAuthenticated();
+    };
+
+    var setUser = function () {
+        AuthService.getLoggedInUser().then(function (user) {
+           $scope.user = user;
+        });
+    };
+
+    setUser();
+
+    // $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+
+})
