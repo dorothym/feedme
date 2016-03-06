e89883b6a1dc4482ed2aa988e0ae822a57f5d4ac
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-03-06 00:58:50 -0500

    User list is displayed

--- browser/js/admin/admin.js
+++ browser/js/admin/admin.js
@@ -3,13 +3,40 @@ app.config(function ($stateProvider) {
     $stateProvider.state('admin', {
         url: '/admin',
         templateUrl: 'js/admin/admin.html',
-        controller: 'AdminCtrl'
+        controller: 'AdminCtrl', 
+        resolve: {
+        	allUsers: function(AdminFactory) {
+        		return AdminFactory.fetchAllUsers();
+        	}
+        }
     });
 
 });
 
-app.controller('AdminCtrl', function ($scope, AuthService, $state) {
-
-    // $scope.foo = {};
+app.controller('AdminCtrl', function ($scope, AuthService, $state, allUsers, AdminFactory) {
+	$scope.allUsers = allUsers;
+    $scope.removeUser = function() {
+    	AdminFactory.removeUser();
+    }
 
 });
+
+app.factory('AdminFactory', function($http) {
+	var AdminFactory = {};
+
+	AdminFactory.fetchAllUsers = function() {
+		return $http.get('/api/users')
+		.then(function(res) {
+			return res.data;
+		})
+	}
+
+	AdminFactory.removeUser = function(id) {
+		return $http.delete('/api/users/' + id)
+		.then(function(res) {
+			return res.data;
+		})
+	}
+
+	return AdminFactory;
+})
