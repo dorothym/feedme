adbcbd9fe2a4e3416753b6a1b55c0d5dda1f7eb4
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-03-07 22:39:00 -0500

    fixed the user type and manage admin btn on admin page

--- browser/js/admin/admin.js
+++ browser/js/admin/admin.js
@@ -19,6 +19,17 @@ app.controller('AdminCtrl', function ($scope, AuthService, $state, allUsers, Adm
 	$scope.updated = false;
 	$scope.showForm = false;
 	$scope.updatedUser ={};
+    $scope.userType = function(user) {
+        if(user.type === "Chef" && user.admin) {
+            return "Chef / Admin"
+        } else if(user.type === "Chef") {
+            return "Chef"
+        } else if(user.admin) {
+            return "Admin"
+        } else {
+            return "Buyer"
+        }
+    }
 
 	$scope.showUpdateForm = function(user) {
 		$scope.showForm = true;
@@ -30,7 +41,6 @@ app.controller('AdminCtrl', function ($scope, AuthService, $state, allUsers, Adm
     	AdminFactory.removeUser(user);
     }
 
-
     $scope.updateUser = function(user, data) {
     	$scope.updated = true;
     	$scope.action = "updated";
@@ -38,9 +48,13 @@ app.controller('AdminCtrl', function ($scope, AuthService, $state, allUsers, Adm
     }
 
     $scope.assignAdmin = function(user) {
-    	$scope.updated = true;
-    	$scope.action = "assign as an admin";
-    	AdminFactory.assignAdmin(user);
+        $scope.updated = true;
+        AdminFactory.assignAdmin(user);
+        if(!user.admin) {
+            $scope.action = "assigned as an admin";
+        } else  {
+            $scope.action = "removed as an admin";
+        }
     }
 
 });
@@ -61,7 +75,7 @@ app.factory('AdminFactory', function($http) {
 		.then(function(res) {
 			return res.data;
 		})
-         .then(setCache)
+         // .then(setCache)
 	}
 
 	AdminFactory.removeUser = function(user) {
@@ -72,8 +86,15 @@ app.factory('AdminFactory', function($http) {
 	}
  	
  	AdminFactory.assignAdmin = function(user) {
-		return $http.put('/api/users/' + user._id, {admin: true})
+            if(!user.admin)
+    		    return $http.put('/api/users/' + user._id, {admin: true})
+            else 
+                return $http.put('/api/users/' + user._id, {admin: false})    
 	}
+
+    AdminFactory.removeAdmin = function(user) {
+        return $http.put('/api/users/' + user._id, {admin: false})
+    }
 
 	AdminFactory.updateUser = function(user, data) {
 		return $http.put('/api/users/' + user._id, data)
